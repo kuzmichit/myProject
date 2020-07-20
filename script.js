2b17c5e616d4b5257253c9fac8dcad761b062188
kuzmichit <68516902+kuzmichit@users.noreply.github.com>
2020-07-20 20:49:27 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+'use strict';
+
+function startQuiz () {
+  document.querySelector( 'button' ).classList.add( 'hidden' );
+   let randomNumber = Math.floor( Math.random() * 100 );
+   let questionElem =  document.querySelectorAll( 'p' )[randomNumber];  
+
+  questionElem.classList.remove( 'hidden' );
+
+ }
